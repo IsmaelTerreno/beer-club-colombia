d680f345a2f742650b165c7eb64e187f89b63f3d
IsmaelTerreno <elismakof2002@gmail.com>
2024-11-16 18:30:53 -0300

    Update Item model with docstring and rename identifier
    Added a docstring to the Item class for better documentation. Renamed 'id' to 'id_item' to clarify it represents a unique identifier for the item.

--- model/Item.py
+++ model/Item.py
@@ -2,7 +2,13 @@ from pydantic import BaseModel
 
 
 class Item(BaseModel):
-    id: int
+    """
+    Represents an item with an identifier and a name.
+
+    Detailed description of the class, its purpose, and usage.
+
+    :ivar id_item: Unique identifier for the item.
+    :ivar name: Name of the item.
+    """
+    id_item: int
     name: str
-    price_per_unit: int
-    total: int
