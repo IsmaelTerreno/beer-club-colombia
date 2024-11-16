eda9b87ff1fa308739080c6840e69e91dfa3ff49
IsmaelTerreno <elismakof2002@gmail.com>
2024-11-16 18:33:07 -0300

    Update stock service calls in create_order logic
    Refactor `create_order` to use `get_stock_by_id` instead of `get_stock_by_name`, and adapt the iteration variable name. Additionally, set the `paid` status directly within the order processing logic based on stock availability.

--- service/order_service.py
+++ service/order_service.py
@@ -1,6 +1,6 @@
 from model.Order import Order, StatusOrder
 from repository.order_repository import order_repository
-from service.stock_service import subtraction_stock, get_stock_by_name
+from service.stock_service import subtraction_stock, get_stock_by_id
 
 
 def create_order(order: Order) -> Order:
@@ -15,9 +15,9 @@ def process_order(order: Order) -> Order:
     # Start processing the order rounds
     for currentRound in order.rounds:
         # Take each item from the current round
-        for currentItem in currentRound.items:
+        for currentItem in currentRound.selected_items:
             # Check if the stock is available
-            stock = get_stock_by_name(currentItem.name)
+            stock = get_stock_by_id(currentItem.id_item)
             # If the stock is available and the quantity is enough, subtract the stock
             if stock is not None and stock.quantity >= currentItem.quantity:
                 # Subtract the stock
@@ -25,13 +25,14 @@ def process_order(order: Order) -> Order:
                 # Set the order status to COMPLETE
                 order.status = StatusOrder.COMPLETED
                 order.details = "Order processed successfully"
+                order.paid = True
             else:
                 # If the stock is not available or the quantity is not enough, set the order status to FAILED
                 order.status = StatusOrder.FAILED
                 order.details = "Out of stock"
-                # Update the order
-                order_repository.update_order(order)
-                return order
+                order.paid = False
+            # Update the order
+            order_repository.update_order(order)
     return order
 
 
