f660490a1524bcd9c45d9bc2255c01fbc1cef619
IsmaelTerreno <elismakof2002@gmail.com>
2024-11-16 18:38:38 -0300

    Add test for creating and processing orders
    Added a new test, `test_should_create_and_process_order`, to ensure the API correctly handles order creation and subsequent processing. This includes verifying correct status codes and response payload. Also made minor adjustments to existing test comments for clarity.

--- test/test_order_endpoint_e2e.py
+++ test/test_order_endpoint_e2e.py
@@ -12,13 +12,13 @@ async def test_should_create_order():
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
         # Define the order payload
         order_payload = generate_order_payload(1)
-
+        # Create the order
         response = await ac.post("/api/v1/order", json=order_payload)
-
+        # Assert the response with the expected values
         assert response.status_code == 201
         assert response.json() == {
             "message": "Order created successfully",
-            "data": order_payload,  # This should match the data format returned by `order.dict()`
+            "data": order_payload,
         }
 
 
@@ -39,7 +39,7 @@ async def test_should_create_and_find_existing_order():
         assert response.status_code == 200
         assert response.json() == {
             "message": "Order found",
-            "data": order_payload,  # This should match the data format returned by `order.dict()`
+            "data": order_payload,
         }
 
 
@@ -60,7 +60,7 @@ async def test_should_create_and_delete_existing_order():
         assert response.status_code == 202
         assert response.json() == {
             "message": "Order deleted",
-            "data": order_payload,  # This should match the data format returned by `order.dict()`
+            "data": order_payload,
         }
 
         # Find the order
@@ -72,6 +72,27 @@ async def test_should_create_and_delete_existing_order():
         }
 
 
+@pytest.mark.asyncio
+async def test_should_create_and_process_order():
+    async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
+        # Define the order payload id
+        id_to_test = 4
+        # Define the order payload
+        order_payload = generate_order_payload(id_to_test)
+
+        # Create the order
+        response = await ac.post("/api/v1/order", json=order_payload)
+        assert response.status_code == 201
+
+        # Process the order
+        response = await ac.post("/api/v1/order/process", json=order_payload)
+        assert response.status_code == 200
+        assert response.json() == {
+            "message": "Order processed successfully",
+            "data": order_payload,
+        }
+
+
 def generate_order_payload(order_id: int):
     return {
         "id": order_id,
@@ -80,27 +101,36 @@ def generate_order_payload(order_id: int):
         "subtotal": 100.0,
         "taxes": 20.0,
         "discounts": 5.0,
-        "items": [
+        "cash_tendered": 200.0,
+        "option_items": [
             {
-                "id": 1,
-                "name": "Item 1",
-                "price_per_unit": 25,
-                "total": 50,
+                "id_item": 1,
+                "name": "Corona",
             },
             {
-                "id": 2,
-                "name": "Item 2",
-                "price_per_unit": 25,
-                "total": 50,
+                "id_item": 2,
+                "name": "Quilmes",
             },
         ],
         "rounds": [
             {
                 "id": 1,
                 "created": "2023-10-05T15:26:48.123456",
-                "items": [
-                    {"id": 1, "name": "Item 1", "price_per_unit": 25, "total": 50},
-                    {"id": 2, "name": "Item 2", "price_per_unit": 25, "total": 50},
+                "selected_items": [
+                    {
+                        "id": 1,
+                        "id_item": 1,
+                        "quantity": 2,
+                        "price_per_unit": 0,
+                        "sub_total": 0,
+                    },
+                    {
+                        "id": 2,
+                        "id_item": 2,
+                        "quantity": 1,
+                        "price_per_unit": 0,
+                        "sub_total": 0,
+                    },
                 ],
             }
         ],
