5f6b9a399390a1e660bfff9a0fcc0ed9fe06351e
IsmaelTerreno <elismakof2002@gmail.com>
2024-11-16 18:30:35 -0300

    Refactor stock functions to use ID instead of name
    Switched stock retrieval methods from using beer names to beer IDs across subtraction_stock, addition_stock, and get_stock methods. This enhances consistency and potentially improves performance by leveraging unique identifiers.

--- service/stock_service.py
+++ service/stock_service.py
@@ -5,7 +5,7 @@ from repository.stock_repository import stock_repository
 
 def subtraction_stock(stock: Stock) -> Stock:
     for beer in stock_inventory_in_memory_store.beers:
-        stock_beer = stock_repository.get_stock_by_name(beer.name)
+        stock_beer = stock_repository.get_stock_by_id(beer.id)
         if stock_beer is not None:
             stock_beer.quantity -= beer.quantity
     return stock
@@ -13,11 +13,11 @@ def subtraction_stock(stock: Stock) -> Stock:
 
 def addition_stock(stock: Stock) -> Stock:
     for beer in stock_inventory_in_memory_store.beers:
-        stock_beer = stock_repository.get_stock_by_name(beer.name)
+        stock_beer = stock_repository.get_stock_by_id(beer.id)
         if stock_beer is not None:
             stock_beer.quantity += beer.quantity
     return stock
 
 
-def get_stock_by_name(name: str) -> Stock:
-    return stock_repository.get_stock_by_name(name)
+def get_stock_by_id(stock_id: int) -> Stock:
+    return stock_repository.get_stock_by_id(stock_id)
