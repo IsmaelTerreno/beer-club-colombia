c9406e1bd7485e62c4d7f33f6fdc3ae63196bb71
IsmaelTerreno <elismakof2002@gmail.com>
2024-11-19 01:22:10 -0300

    Add CORS middleware to FastAPI app
    This change integrates the CORSMiddleware into the FastAPI application, allowing cross-origin requests from any source. This enables greater flexibility and interoperability when making requests from different domains.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from controller.order_controller import router as order_router
 from controller.stock_controller import router as stock_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(order_router)
 app.include_router(stock_router)
